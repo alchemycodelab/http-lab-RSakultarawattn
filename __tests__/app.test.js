7b78d3575b53f940dcd728ecbe46249cded770d7
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-14 15:27:19 -0800

    get test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,11 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('should GET spot', async() => {
+    const res = await request(app)
+      .get('/');
+
+    expect(res.text).toEqual('hi');
+  });
 
 });
