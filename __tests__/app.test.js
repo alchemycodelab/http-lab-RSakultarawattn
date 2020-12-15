fa214fa10ab430bc680ad21af230b1e6ea3ee51e
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-14 16:37:25 -0800

    red passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,4 +17,25 @@ describe('app routes', () => {
     expect(res.text).toEqual('hi');
   });
 
+  it('should return html with the word red', async() => {
+    const res = await request(app)
+      .get('/red');
+
+    expect(res.text).toEqual('<h1>red</h1>');
+  });
+  it('should return html with the word green', async() => {
+    const res = await request(app)
+      .get('/green');
+
+    expect(res.text).toEqual('<h1>green</h1>');
+  });
+
+  it('should return html with the word blue', async() => {
+    const res = await request(app)
+      .get('/blue');
+
+    expect(res.text).toEqual('<h1>blue</h1>');
+  });
+
+
 });
